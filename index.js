2d647a6cb41b5c0fae8a06afcc33c6acc611b780
Jirka Martinec <jirka@martinec.dev>
2023-07-20 22:46:41 +0200

    init

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log('hi');
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log('yes');
+    } else if (this.readyState == 4 && this.status == 401) {
+        console.log('no');
+    }
+};
+xhttp.open("GET", "https://www.sauto.cz/api/v1/users/self", true);
+xhttp.withCredentials = true;
+xhttp.send();
+
+function getCookiesFromResponse() {
+    var xhttp2 = new XMLHttpRequest();
+    xhttp2.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = this.responseText;
+            var session = response.match(/(?<=ds=).{72}/)[0];
+            console.log(session);
+        }
+    };
+    xhttp2.open("GET", "https://www.sauto.cz", true);
+    xhttp2.withCredentials = true;
+    xhttp2.send();
+}
