56a09f5cb67b617f33c1e6ef73be915f7391e125
Jirka Martinec <jirka@martinec.dev>
2023-07-21 20:44:04 +0200

    init

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ async function getCookies()
             'https://www.sauto.cz',
             {
                 withCredentials: true,
+                credentials: "same-origin",
+                // credentials: "include",
             },
         );
 
