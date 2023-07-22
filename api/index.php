34556e172ce53cf56dbd1aae7209dd5c55e07a0f
Jirka Martinec <jirka@martinec.dev>
2023-07-22 18:25:38 +0200

    init

--- api/index.php
+++ api/index.php
@@ -1,8 +1,11 @@
 <?php
 
 header("Access-Control-Allow-Origin: https://www.sauto.cz");
+header('Access-Control-Max-Age: 5');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization, Set-Cookie');
 header('Access-Control-Allow-Credentials: true');
 
+//header("Content-Type: text/plain");
 echo "<pre>";
 print_r(getallheaders());
