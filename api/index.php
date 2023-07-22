6b54c6b478569e11eba888a7aefcdaeebdf36e1b
Jirka Martinec <jirka@martinec.dev>
2023-07-22 18:08:58 +0200

    init

--- api/index.php
+++ api/index.php
@@ -1,3 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://www.sauto.cz");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Credentials: true');
